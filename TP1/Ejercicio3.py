cc21edaba5209682094c5358010e9234331ed730
azulzaietz <azulzaietz@gmail.com>
2020-05-17 15:20:27 -0300

    cambio nombre f y defino F

--- TP1/Ejercicio3.py
+++ TP1/Ejercicio3.py
@@ -4,10 +4,20 @@ import math
 
 #Graficar la función de densidad de probabilidad
 
-def my_dist(x):
+def f(x):
     return (13/12*math.pi) - (x**2)/(math.pi)**3
 
 x = np.arange(-math.pi/2, math.pi/2)
-p = my_dist(x)
+p = f(x)
 plt.plot(x, p)
 plt.show()
+
+#Calcular y graficar la función de probabilidad acumulada y su inversa
+
+#Función de probabilidad acumulada (Función de distribución):
+def F(x):
+    if x < (-math.pi/2):
+        return 0
+    elif x >= -math.pi/2 and x <= math.pi/2:
+        return (13*x/12*math.pi)-(x**3/3*(math.pi**3))
+    return 1
