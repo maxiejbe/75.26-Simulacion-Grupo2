6bd62307bc274e00c9e8e17bec5c5ee3fefea33d
azulzaietz <azulzaietz@gmail.com>
2020-05-17 14:38:54 -0300

    Punto a

--- TP1/Ejercicio3.py
+++ TP1/Ejercicio3.py
@@ -1,2 +1,13 @@
 import numpy as np 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+import math
+
+#Graficar la función de densidad de probabilidad
+
+def my_dist(x):
+    return (13/12*math.pi) - (x**2)/(math.pi)**3
+
+x = np.arange(-math.pi/2, math.pi/2)
+p = my_dist(x)
+plt.plot(x, p)
+plt.show()
