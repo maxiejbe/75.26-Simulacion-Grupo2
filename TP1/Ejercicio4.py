3629bf6099db551f103905fc8483d57a236b7773
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-05-20 22:38:31 -0300

    Finishing exercise 4a

--- TP1/Ejercicio4.py
+++ TP1/Ejercicio4.py
@@ -1,15 +1,22 @@
+# coding=utf-8
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 
 def generateNormal(n, mu, sigma):
-    return np.random.normal(mu, sigma, n)
+    numbers = np.array([])
+    acceptedN = 0
+    while acceptedN <= n:
+        x = np.random.normal(mu, sigma)
+        y = np.random.normal(mu, sigma)
+        if y < x:
+            numbers = np.append(numbers, y)
+            acceptedN = acceptedN + 1
+    return numbers
 
 
-# mean and standard deviation
+# Aplicando el algoritmo de Aceptación y rechazo se pide:
+# a) Generar 100.000 número aleatorios con distribución Normal de media 25 y desvío estándar 2 .
 n, mu, sigma = 100000, 25, 2
 numbers = generateNormal(n, mu, sigma)
-
-print(len(numbers))
-plt.hist(numbers, bins="sturges")
-plt.show()
