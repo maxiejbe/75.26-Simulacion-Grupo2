6cd50c50b8b6f1063adb81929c0ed82788016945
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-05-20 22:39:03 -0300

    Finishing exercise 4b

--- TP1/Ejercicio4.py
+++ TP1/Ejercicio4.py
@@ -16,7 +16,20 @@ def generateNormal(n, mu, sigma):
     return numbers
 
 
+def drawHistogram(numbers, show):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.hist(numbers, weights=np.zeros_like(numbers) + 1.0 / numbers.size)
+    ax.set_xlabel("Numbers range")
+    ax.set_ylabel("Relative frequency")
+    if show:
+        plt.show()
+
+
 # Aplicando el algoritmo de Aceptación y rechazo se pide:
 # a) Generar 100.000 número aleatorios con distribución Normal de media 25 y desvío estándar 2 .
 n, mu, sigma = 100000, 25, 2
 numbers = generateNormal(n, mu, sigma)
+
+# b) Realizar un histograma de frecuencias relativas con todos los valores obtenidos.
+drawHistogram(numbers, True)
