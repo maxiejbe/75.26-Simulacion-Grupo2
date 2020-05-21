32b1688ba63c2bc7f56dfc033fae67f88ed9822c
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-05-20 22:39:21 -0300

    Finishing exercise 4c

--- TP1/Ejercicio4.py
+++ TP1/Ejercicio4.py
@@ -26,10 +26,21 @@ def drawHistogram(numbers, show):
         plt.show()
 
 
+def drawNormalDistribution(mu, sigma, show):
+    x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
+    plt.plot(x, stats.norm.pdf(x, mu, sigma), "-r")
+    if show:
+        plt.show()
+
+
 # Aplicando el algoritmo de Aceptación y rechazo se pide:
 # a) Generar 100.000 número aleatorios con distribución Normal de media 25 y desvío estándar 2 .
 n, mu, sigma = 100000, 25, 2
 numbers = generateNormal(n, mu, sigma)
 
 # b) Realizar un histograma de frecuencias relativas con todos los valores obtenidos.
-drawHistogram(numbers, True)
+drawHistogram(numbers, False)
+
+# c) Comparar, en el mismo gráfico, el histograma realizado en el punto anterior con la función de densidad de
+# probabilidad brindada por el lenguaje elegido (para esta última distribución utilizar un gráfico de línea).
+drawNormalDistribution(mu, sigma, True)
