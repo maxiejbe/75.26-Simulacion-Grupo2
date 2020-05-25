bf1e65e18a07df100e4dc5f42d32d30979f0ba9a
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-05-25 01:29:32 -0300

    Adjusting exercise4 output

--- TP1/Ejercicio4.py
+++ TP1/Ejercicio4.py
@@ -5,12 +5,12 @@ import scipy.stats as stats
 import random as rn
 
 
-def generateNormalAcceptanceRejection(n, mu, sigma):
+def generateNormalAcceptanceRejection(n, mu, sigma, output):
     deltax = 0.5 * mu
     xmin, xmax = mu - deltax, mu + deltax
     ymax = max(stats.norm.pdf(np.arange(xmin, xmax), mu, sigma))
     numbers = np.array([])
-    acceptedN = 0
+    acceptedN, rejectedN = 0, 0
     while acceptedN < n:
         x = np.random.uniform(xmin, xmax)
         y = np.random.uniform(0.0, ymax)
@@ -20,6 +20,13 @@ def generateNormalAcceptanceRejection(n, mu, sigma):
             numbers = np.append(numbers, x)
             acceptedN += 1
             continue
+        else:
+            rejectedN += 1
+
+    if output:
+        print("Y Max={}".format(ymax))
+        print("Cantidad aceptados={}".format(acceptedN))
+        print("Cantidad rechazados={}".format(rejectedN))
 
     return numbers
 
@@ -34,18 +41,17 @@ def drawHistogram(numbers, show):
         plt.show()
 
 
-def drawNormalDistribution(mu, sigma, show):
+def drawNormalDistribution(mu, sigma):
     x = np.linspace(mu - 3 * sigma, mu + 3 * sigma, 100)
     plt.plot(x, stats.norm.pdf(x, mu, sigma), "-r")
-    if show:
-        plt.show()
+    plt.show()
 
 
 def main():
     # Aplicando el algoritmo de Aceptación y rechazo se pide:
     # a) Generar 100.000 número aleatorios con distribución Normal de media 25 y desvío estándar 2 .
-    n, mu, sigma = 1000000, 25, 2
-    numbers = generateNormalAcceptanceRejection(n, mu, sigma)
+    n, mu, sigma = 100000, 25, 2
+    numbers = generateNormalAcceptanceRejection(n, mu, sigma, True)
 
     # b) Realizar un histograma de frecuencias relativas con todos los valores obtenidos.
     drawHistogram(numbers, True)
@@ -53,16 +59,15 @@ def main():
     # c) Comparar, en el mismo gráfico, el histograma realizado en el punto anterior con la función de densidad de
     # probabilidad brindada por el lenguaje elegido (para esta última distribución utilizar un gráfico de línea).
     drawHistogram(numbers, False)
-    drawNormalDistribution(mu, sigma, True)
+    drawNormalDistribution(mu, sigma)
 
     # d) Calcular la media y la varianza de la distribución obtenida y compararlos con los valores teóricos
-    print("Valores teóricos:")
-    print("Media:", mu)
-    print("Varianza:", sigma ** 2)
-
-    print("Valores prácticos:")
-    print("Media:", np.average(numbers))
-    print("Varianza:", np.var(numbers))
+    print("Valores teóricos: Media={},Varianza={}".format(mu, sigma ** 2))
+    print(
+        "Valores prácticos: Media={},Varianza={}".format(
+            np.average(numbers), np.var(numbers)
+        )
+    )
 
 
 if __name__ == "__main__":
