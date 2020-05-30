09bd9be81fa9ce8b25d65b6f4cfc16d93e5e77c7
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-05-30 20:21:16 -0300

    Removing unnecessary term separation

--- TP1/Ejercicio4.py
+++ TP1/Ejercicio4.py
@@ -13,7 +13,7 @@ def generate_normal_acceptance_rejection(n, mu, sigma, output):
 
 def generate_standard_normal_acceptance_rejection(n, output):
     # Theory: Derive Fx(t)/Fy(t) => t=1
-    c = 2 * (stats.norm.pdf(1)) / stats.expon.pdf(1)
+    c = 2 * stats.norm.pdf(1) / stats.expon.pdf(1)
 
     numbers = np.array([])
     accepted_n, rejected_n = 0, 0
