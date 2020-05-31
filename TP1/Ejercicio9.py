a8c6c5c0c7a88692b5b59e29f8905a8a7e9c791b
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-05-31 17:32:09 -0300

    Validating x,y position boundaries

--- TP1/Ejercicio9.py
+++ TP1/Ejercicio9.py
@@ -13,13 +13,21 @@ class Direction(Enum):
 
 class Board:
     def __init__(
-        self, size_x, size_y, green_particles_count, red_particles_count, moving_time
+        self,
+        size_x,
+        size_y,
+        green_particles_count,
+        red_particles_count,
+        moving_time,
+        particle_step,
     ):
         self.size_x = size_x
         self.size_y = size_y
 
         self.time = 0
         self.moving_time = moving_time
+
+        self.particle_step = particle_step
 
         self.green_particles_count = green_particles_count
         self.green_particles = []
@@ -29,17 +37,23 @@ class Board:
 
         self.initialize_particles()
 
+    def get_particle_step(self):
+        return self.particle_step
+
+    def get_size(self):
+        return self.size_x, self.size_y
+
     def initialize_green_particles(self):
         for i in range(1, self.green_particles_count):
             x = np.random.uniform(0, self.size_x / 2)
             y = np.random.uniform(0, self.size_y)
-            self.green_particles.append(Particle(x, y))
+            self.green_particles.append(Particle(x, y, self))
 
     def initialize_red_particles(self):
         for i in range(1, self.red_particles_count):
             x = np.random.uniform(self.size_x / 2, self.size_x)
             y = np.random.uniform(0, self.size_y)
-            self.red_particles.append(Particle(x, y))
+            self.red_particles.append(Particle(x, y, self))
 
     def initialize_particles(self):
         # Move maybe this logic to particle
@@ -47,13 +61,14 @@ class Board:
         self.initialize_red_particles()
 
     def start_movement(self):
-        while self.time <= self.moving_time:
+        while self.time < self.moving_time:
             for p in self.green_particles:
                 p.move()
             for p in self.red_particles:
                 p.move()
             self.time += 1
             self.draw()
+        plt.show()
 
     def draw(self):
         plt.cla()
@@ -73,27 +88,47 @@ class Board:
         plt.legend(
             fancybox=True, framealpha=1, shadow=True, borderpad=1, loc="upper center"
         )
-        plt.pause(0.001)
+        plt.pause(0.0001)
 
 
 class Particle:
-    def __init__(self, x, y):
+    def __init__(self, x, y, board):
         self.x = x
         self.y = y
         self.directions = self.generate_directions()
-        self.particle_step = 0.1
+        self.particle_step = board.get_particle_step()
+        self.boundary_x, self.boundary_y = board.get_size()
+
+    def validate_boundaries(self, x, y):
+        if x < 0:
+            return False
+        if x > self.boundary_x:
+            return False
+        if y < 0:
+            return False
+        if y > self.boundary_y:
+            return False
+        return True
 
     def move_to_north(self):
-        self.y += self.particle_step
+        new_y = self.y + self.particle_step
+        if self.validate_boundaries(self.x, new_y):
+            self.y = new_y
 
     def move_to_south(self):
-        self.y -= self.particle_step
+        new_y = self.y - self.particle_step
+        if self.validate_boundaries(self.x, new_y):
+            self.y = new_y
 
     def move_to_east(self):
-        self.x += self.particle_step
+        new_x = self.x + self.particle_step
+        if self.validate_boundaries(new_x, self.y):
+            self.x = new_x
 
     def move_to_west(self):
-        self.x -= self.particle_step
+        new_x = self.x - self.particle_step
+        if self.validate_boundaries(new_x, self.y):
+            self.x = new_x
 
     def generate_directions(self):
         direction_actions = []
@@ -117,7 +152,8 @@ class Particle:
 def main():
     green_particles_count, red_particles_count = 100, 100
     board_size_x, board_size_y = 10, 20
-    moving_time = 3000
+    particle_step = 0.1
+    moving_time = 100
 
     board = Board(
         board_size_x,
@@ -125,6 +161,7 @@ def main():
         green_particles_count,
         red_particles_count,
         moving_time,
+        particle_step,
     )
     board.start_movement()
 
