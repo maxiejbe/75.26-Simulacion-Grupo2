de382bd72a6bcde603836f4a0106e6de519eb2c7
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-06-01 18:14:49 -0300

    No boundaries, growing during the animation

--- TP1/Ejercicio8.py
+++ TP1/Ejercicio8.py
@@ -2,40 +2,48 @@ import numpy as np
 import random
 from matplotlib import pyplot as plt
 
-#Cantidad de pasos
+# Cantidad de pasos
 N_steps = 1000
 
-#Probabilidad: equiprobable para moverse en cualquiera de las 4 direcciones
-#(arriba, abajo, derecha o izquierda)
+# Probabilidad: equiprobable para moverse en cualquiera de las 4 direcciones
+# (arriba, abajo, derecha o izquierda)
 prob = 0.25
+
+# board_size_x = 50
+# board_size_y = 50
+
 
 def RandomWalk(N):
 
     position = []
     x = 0
     y = 0
-    position.append((x,y))
+    position.append((x, y))
 
-    for i in range(1,N):
-        #Probabilidad random entre 0 y 1
+    for i in range(1, N):
+        # Probabilidad random entre 0 y 1
         p = random.random()
-        #Modifica posicion segun corresponda
+        # Modifica posicion segun corresponda
         if p < 0.25:
-          x += 1
+            x += 1
         elif 0.25 <= p < 0.5:
-          y += 1
+            y += 1
         elif 0.5 <= p < 0.75:
-          x -= 1
+            x -= 1
         elif 0.75 <= p < 1:
-          y -= 1
+            y -= 1
 
-        #Agrega coordenadas movimiento a la lista de posiciones
-        position.append((x,y))
+        # Agrega coordenadas movimiento a la lista de posiciones
+        position.append((x, y))
+        plt.plot(*zip(*position))
+        # plt.xlim(-board_size_x, board_size_x)
+        # plt.ylim(-board_size_y, board_size_y)
+        plt.xlabel("x")
+        plt.ylabel("y")
+        plt.pause(0.001)
 
-    plt.plot(*zip(*position))
-    plt.xlabel('x')
-    plt.ylabel('y')
     plt.show()
     return
+
 
 RandomWalk(N_steps)
