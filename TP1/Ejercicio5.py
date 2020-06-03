7e1381730e8ad7c89d443b3eea7322ab8f9c5a36
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-06-02 23:43:19 -0300

    Fixing gap size and chi2 assert for exercise 5

--- TP1/Ejercicio5.py
+++ TP1/Ejercicio5.py
@@ -8,47 +8,52 @@ from Ejercicio1 import gcl01, gcl
 # Analizar el resultado obtenido, e indicar si la distribución de probabilidades pasa o no el test.
 # Considerar un nivel de significación del 1%.
 
-def gapTest(numberOfGaps, alpha, beta):
-    i = 0
-    gap = 0
-    gaps = np.zeros(numberOfGaps)
+
+def gap_test(number_of_gaps, alpha, beta):
+    current_gap = 0
+    gaps = np.zeros(number_of_gaps)
     number = gcl01(1)
-    while i < numberOfGaps:
-        if((number > alpha) and (number <= beta)):
-            gap += 1
+    while current_gap < number_of_gaps:
+        if (number > alpha) and (number <= beta):
+            current_gap += 1
         else:
-            np.append(gaps, gap + 1)
-            gaps[i] = gap + 1
-            i += 1
-            gap = 0
-        number_gcl = gcl(1, x0 = number)
-        number = gcl01(1, x0 = number_gcl)
+            gaps[current_gap] = gaps[current_gap] + 1
+        number_gcl = gcl(1, x0=number)
+        number = gcl01(1, x0=number_gcl)
 
-    #probabilidades esperadas
+    # probabilidades esperadas
     p = beta - alpha
-    expected = np.zeros(numberOfGaps)
     j = 0
-    while j < len(gaps):
-        num = gaps[j]
-        expected[j] = p*((1-p)**(num-1))*(numberOfGaps)
-        j += 1
+    max_gap_size = int(max(gaps)) + 1
+    observed_p = np.zeros(max_gap_size)
+    expected_p = np.zeros(max_gap_size)
 
-    [h,pValor] = stats.chisquare(gaps, expected)
+    while j < max_gap_size:
+        number_of_occ = np.count_nonzero(gaps == j)
+        observed_p[j] = number_of_occ / number_of_gaps
+        expected_p[j] = p * ((1 - p) ** (j))
+        j += 1
+    [h, pValor] = stats.chisquare(observed_p, expected_p)
     return pValor, gaps
 
-[pValor, gaps] = gapTest(100000, 0.2, 0.5)
-plt.hist(gaps, bins = 50, color = 'purple')
-plt.title('Histograma de la distribución de las longitudes de gaps para 100.000 gaps generados')
-plt.xlabel('Gaps')
-plt.ylabel('Cantidad de gaps')
+
+[pValor, gaps] = gap_test(100000, 0.2, 0.5)
+plt.hist(gaps, bins=50, color="purple")
+plt.xlabel("Tamaño de gaps")
+plt.ylabel("Frecuencia")
 plt.show()
 maxgap = max(gaps)
-print("El gap mas largo fue: ", maxgap)
-nivelDeSignifiacion = 0.01
-if pValor < nivelDeSignifiacion:
-    print("Para un nivel de significación del ", nivelDeSignifiacion*100, "%, obtenemos que podemos rechazar la hipótesis nula (h=0)")
+print("El gap de tamaño máximo fue: ", maxgap)
+alpha = 0.01
+if pValor < alpha:
+    print(
+        "Para un nivel de significación del ",
+        alpha * 100,
+        "%, obtenemos que podemos rechazar la hipótesis nula (h=0)",
+    )
 else:
-    print("Para un nivel de significación del ", nivelDeSignifiacion*100, "%, obtenemos que no podemos rechazar la hipótesis nula (h=0)")
-
-
-
+    print(
+        "Para un nivel de significación del ",
+        alpha * 100,
+        "%, obtenemos que no podemos rechazar la hipótesis nula (h=0)",
+    )
