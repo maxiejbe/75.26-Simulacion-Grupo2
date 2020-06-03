fab5e23f60eef8dfbab15e1a9077463fc622715d
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-06-02 23:36:45 -0300

    Formatting exercises 1 to 4

--- TP1/Ejercicio2.py
+++ TP1/Ejercicio2.py
@@ -5,12 +5,12 @@ from Ejercicio1 import gcl
 
 
 def generate_rn(n):
-    gcl_values = (gcl(2*n) % 6)
+    gcl_values = gcl(2 * n) % 6
     result = np.array([])
     i = 0
     while i < n:
-        j = 2*i
-        suma = np.take(gcl_values, j) + np.take(gcl_values, j+1) 
+        j = 2 * i
+        suma = np.take(gcl_values, j) + np.take(gcl_values, j + 1)
         result = np.append(result, suma)
         i = i + 1
     return result
@@ -22,6 +22,7 @@ def main():
     plt.hist(data, bins=10)
     plt.show()
     return
+
 
 if __name__ == "__main__":
     main()
--- TP1/Ejercicio3.py
+++ TP1/Ejercicio3.py
@@ -5,53 +5,58 @@ import numpy as np
 from scipy import interpolate
 from Ejercicio1 import gcl01
 
-#Graficar la función de densidad de probabilidad
+# Graficar la función de densidad de probabilidad
+
 
 def f(x):
-    return (13/(12*math.pi)) - (x**2)/((math.pi)**3)
+    return (13 / (12 * math.pi)) - (x ** 2) / ((math.pi) ** 3)
 
-x = np.arange(-math.pi/2, math.pi/2)
+
+x = np.arange(-math.pi / 2, math.pi / 2)
 p = f(x)
-plt.plot(x, p, label='linear')
-plt.title('Función de densidad de probabilidad')
+plt.plot(x, p, label="linear")
+plt.title("Función de densidad de probabilidad")
 plt.show()
 
-#Calcular y graficar la función de probabilidad acumulada y su inversa
+# Calcular y graficar la función de probabilidad acumulada y su inversa
 
-#Función de probabilidad acumulada (Función de distribución):
+# Función de probabilidad acumulada (Función de distribución):
 def F(x):
-    return ((13*x)/(12*np.pi))-((x**3)/(3*(np.pi**3))) + 0.5
+    return ((13 * x) / (12 * np.pi)) - ((x ** 3) / (3 * (np.pi ** 3))) + 0.5
+
 
 def main():
-    #Grafico funcion distribucion original
+    # Grafico funcion distribucion original
     x = np.linspace(-3, 3, 30)
     y = [F(xi) for xi in x]
-    plt.plot(x, y, '-r')
-    plt.title('Función de probabilidad acumulada')
+    plt.plot(x, y, "-r")
+    plt.title("Función de probabilidad acumulada")
     plt.show()
 
-    #inviersión de los ejes
+    # inviersión de los ejes
     x, y = y, x
 
-    #interpolación con puntos equiespaciados
+    # interpolación con puntos equiespaciados
     F_inversa = interpolate.interp1d(x, y)
 
-    #Gráfico de la inversa
-    plt.plot(x, y, 'o', x, F_inversa(x), '-')
-    plt.title('Función de distribución inversa')
+    # Gráfico de la inversa
+    plt.plot(x, y, "o", x, F_inversa(x), "-")
+    plt.title("Función de distribución inversa")
     plt.show()
 
-    #Histograma valores generados con gcl01
+    # Histograma valores generados con gcl01
     simulacion_valores(F_inversa)
 
     return
+
 
 def simulacion_valores(funcion):
     x_new = gcl01(100000)
     y_new = funcion(x_new)
     plt.hist(y_new)
-    plt.title('Histograma')
+    plt.title("Histograma")
     plt.show()
     return
+
 
 main()
