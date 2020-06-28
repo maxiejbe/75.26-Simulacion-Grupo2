b37a9f8d8aee5fd1694e02b78533b0201bd45f0b
mariano <mavenesio@gmail.com>
2020-06-28 12:51:32 -0300

    refactor

--- TP2/Ejercicio2.py
+++ TP2/Ejercicio2.py
@@ -22,71 +22,68 @@ class Request:
 
 
 class Server:
-    def __init__(self):
+    def __init__(self, seconds,  p_new_request, p_resolve_request):
         self.state = []
+        self.iterations = int(seconds * 1000/100)
+        self.p_new_request = p_new_request
+        self.p_resolve_request = p_resolve_request
+        self.number_of_request = []
 
     def resolve_request(self):
-        next_start_procesing = False
-        for request in reversed(self.state):
-            if(next_start_procesing):
-                request.start_processing()
-                break
-            if(request.request_status == RequestStatus.Processing):
-                request.end_processing()
-                next_start_procesing = True
+        if(np.random.uniform(0, 1) < self.p_resolve_request):
+            next_start_procesing = False
+            for request in reversed(self.state):
+                if(next_start_procesing):
+                    request.start_processing()
+                    break
+                if(request.request_status == RequestStatus.Processing):
+                    request.end_processing()
+                    next_start_procesing = True
 
     def addNewRequest(self):
-        request = Request()
-        if (len([request for request in self.state if request.request_status == RequestStatus.Processing]) == 0):
-            request.start_processing()
-        self.state.insert(0, request)
+        if (np.random.uniform(0, 1) < self.p_new_request):
+            request = Request()
+            if (len([request for request in self.state if request.request_status == RequestStatus.Processing]) == 0):
+                request.start_processing()
+            self.state.insert(0, request)
 
-    def getNewRequest(self):
+    def get_new_request(self):
         return [request for request in self.state if request.request_status == RequestStatus.New]
 
     def get_processing_request(self):
         return [request for request in self.state if request.request_status == RequestStatus.Processing]
 
-    def getFinalizedRequest(self):
+    def get_finalized_request(self):
         return [request for request in self.state if request.request_status == RequestStatus.Finalized]
 
     def printState(self):
-        print('New ', str(self.getNewRequest()), ' | Processing ', str(
-            self.get_processing_request()), ' | Finalized ',  str(self.getFinalizedRequest()))
+        print('New ', str(self.get_new_request()), ' | Processing ', str(
+            self.get_processing_request()), ' | Finalized ',  str(self.get_finalized_request()))
 
+    def run_server(self):
+        i = 0
+        while i < self.iterations:
+            self.resolve_request()
+            self.addNewRequest()
+            self.number_of_request.append(
+                str(len(self.get_new_request()) + len(self.get_processing_request())))
+            i += 1
 
-def plot_number_request(n, number_of_request):
-    plt.close()
-    x_axis = list(range(0, n))
-    plt.plot(x_axis, number_of_request, color='blue', label='Requests')
-    plt.legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
-    plt.show()
-
-
-def run_simulation(seconds, p_new_request, p_resolve_request):
-    server = Server()
-    number_of_request = []
-    n = seconds*1000/100
-    i = 0
-    while i < n:
-        is_new_request = np.random.uniform(0, 1) < p_new_request
-        is_resolve_request = np.random.uniform(0, 1) < p_resolve_request
-        if (is_resolve_request):
-            server.resolve_request()
-        if (is_new_request):
-            server.addNewRequest()
-        i += 1
-        number_of_request.append(
-            str(len(server.getNewRequest()) + len(server.get_processing_request())))
-
-    plot_number_request(int(n), number_of_request)
+    def plot_number_request(self):
+        plt.close()
+        x_axis = list(range(0, self.iterations))
+        plt.plot(x_axis, self.number_of_request, color='blue')
+        plt.legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
+        plt.show()
 
 
 def main():
     seconds = 1000
     p_new_request = 1/40
     p_resolve_request = 1/30
-    run_simulation(seconds, p_new_request, p_resolve_request)
+    server = Server(seconds, p_new_request, p_resolve_request)
+    server.run_server()
+    server.plot_number_request()
 
 
 if __name__ == "__main__":
