8bb9ba58713af5755056b6f03c469ab071f69086
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-07-01 16:29:57 -0300

    Final formatting

--- TP2/Ejercicio1.py
+++ TP2/Ejercicio1.py
@@ -2,11 +2,13 @@ import numpy as np
 import matplotlib.pyplot as mp
 from enum import Enum
 
+
 class Maquinas(Enum):
     primera = 0
     segunda = 1
 
-#Funcion para dividir las muestras entre las dos maquinas del primer proveedor
+
+# Funcion para dividir las muestras entre las dos maquinas del primer proveedor
 def dividirMuestrasPorMaquina(tiemposMuestras, p):
     tiemposMuestrasPorMaquina = {}
     tiemposMuestrasPorMaquina[Maquinas.primera] = []
@@ -23,20 +25,22 @@ def dividirMuestrasPorMaquina(tiemposMuestras, p):
 
     return tiemposMuestrasPorMaquina
 
+
 # Funcion para calcular la demora
 def calcularDemora(tiemposMuestras, tiemposProveedor, n):
     tiemposEsperaProveedor = np.zeros(n)
     tiempoDeEsperaAnterior = 0
     j = 1
     while j < n:
-        deltaT = tiemposMuestras[j] - (tiemposMuestras[j-1] + tiempoDeEsperaAnterior)
-        tiempoDeEspera =  tiemposProveedor[j-1] - deltaT
+        deltaT = tiemposMuestras[j] - (tiemposMuestras[j - 1] + tiempoDeEsperaAnterior)
+        tiempoDeEspera = tiemposProveedor[j - 1] - deltaT
         if tiempoDeEspera <= 0:
             tiempoDeEspera = 0
         tiemposEsperaProveedor[j] = tiempoDeEspera
         tiempoDeEsperaAnterior = tiempoDeEspera
         j += 1
     return tiemposEsperaProveedor
+
 
 # Calcula los tiempos del proveedor 1 uniendo los tiempos de las dos maquinas
 def calcularDemoraP1(tiemposMuestras, mu1, mu2, p, n):
@@ -45,15 +49,20 @@ def calcularDemoraP1(tiemposMuestras, mu1, mu2, p, n):
     tiemposMuestrasM2 = muestrasPorMaquina[Maquinas.segunda]
 
     tiemposEsperaM1 = np.random.exponential(mu1, len(tiemposMuestrasM1))
-    tiemposProveedorM1 = calcularDemora(tiemposMuestrasM1, tiemposEsperaM1, len(tiemposMuestrasM1))
+    tiemposProveedorM1 = calcularDemora(
+        tiemposMuestrasM1, tiemposEsperaM1, len(tiemposMuestrasM1)
+    )
 
     tiemposEsperaM2 = np.random.exponential(mu2, len(tiemposMuestrasM2))
-    tiemposProveedorM2 = calcularDemora(tiemposMuestrasM1, tiemposEsperaM2, len(tiemposMuestrasM2))
+    tiemposProveedorM2 = calcularDemora(
+        tiemposMuestrasM1, tiemposEsperaM2, len(tiemposMuestrasM2)
+    )
 
     tiemposEspera = np.concatenate((tiemposEsperaM1, tiemposProveedorM2))
     tiemposProveedor = np.concatenate((tiemposProveedorM1, tiemposProveedorM2))
 
     return tiemposEspera, tiemposProveedor
+
 
 def main():
     # Variables
@@ -84,12 +93,24 @@ def main():
     print("N = ", n, "\n")
 
     print("Proveedor 1:")
-    print("El tiempo medio de espera es de: {:.3f} horas".format(tiemposEsperaP1.mean()))
-    print("La fraccion de las solicitudes que no esperaron es: {:.3f}\n".format((n - np.count_nonzero(tiemposEsperaP1))/n))
+    print(
+        "El tiempo medio de espera es de: {:.3f} horas".format(tiemposEsperaP1.mean())
+    )
+    print(
+        "La fraccion de las solicitudes que no esperaron es: {:.3f}\n".format(
+            (n - np.count_nonzero(tiemposEsperaP1)) / n
+        )
+    )
 
     print("Proveedor 2:")
-    print("El tiempo medio de espera es de: {:.3f} horas".format(tiemposEsperaP2.mean()))
-    print("La fraccion de las solicitudes que no esperaron es: {:.3f}\n".format((n - np.count_nonzero(tiemposEsperaP2))/n))
+    print(
+        "El tiempo medio de espera es de: {:.3f} horas".format(tiemposEsperaP2.mean())
+    )
+    print(
+        "La fraccion de las solicitudes que no esperaron es: {:.3f}\n".format(
+            (n - np.count_nonzero(tiemposEsperaP2)) / n
+        )
+    )
 
     print("El proveedor 1 es {:.3f}% veces mas rapido que el 2".format(diferencia))
     if diferencia >= 50:
--- TP2/Ejercicio2.py
+++ TP2/Ejercicio2.py
@@ -23,43 +23,70 @@ class Request:
 
 
 class Server:
-    def __init__(self, seconds,  p_new_request, p_resolve_request):
+    def __init__(self, seconds, p_new_request, p_resolve_request):
         self.state = []
-        self.iterations = int(seconds * 1000/100)
+        self.iterations = int(seconds * 1000 / 100)
         self.p_new_request = p_new_request
         self.p_resolve_request = p_resolve_request
         self.number_of_request_each_time = []
 
     def resolve_request(self):
-        if(np.random.uniform(0, 1) < self.p_resolve_request):
+        if np.random.uniform(0, 1) < self.p_resolve_request:
             next_start_procesing = False
             for request in reversed(self.state):
-                if(next_start_procesing):
+                if next_start_procesing:
                     request.start_processing()
                     break
-                if(request.request_status == RequestStatus.Processing):
+                if request.request_status == RequestStatus.Processing:
                     request.end_processing()
                     next_start_procesing = True
 
     def addNewRequest(self):
-        if (np.random.uniform(0, 1) < self.p_new_request):
+        if np.random.uniform(0, 1) < self.p_new_request:
             request = Request()
-            if (len([request for request in self.state if request.request_status == RequestStatus.Processing]) == 0):
+            if (
+                len(
+                    [
+                        request
+                        for request in self.state
+                        if request.request_status == RequestStatus.Processing
+                    ]
+                )
+                == 0
+            ):
                 request.start_processing()
             self.state.insert(0, request)
 
     def get_new_request(self):
-        return [request for request in self.state if request.request_status == RequestStatus.New]
+        return [
+            request
+            for request in self.state
+            if request.request_status == RequestStatus.New
+        ]
 
     def get_processing_request(self):
-        return [request for request in self.state if request.request_status == RequestStatus.Processing]
+        return [
+            request
+            for request in self.state
+            if request.request_status == RequestStatus.Processing
+        ]
 
     def get_finalized_request(self):
-        return [request for request in self.state if request.request_status == RequestStatus.Finalized]
+        return [
+            request
+            for request in self.state
+            if request.request_status == RequestStatus.Finalized
+        ]
 
     def printState(self):
-        print('New ', str(self.get_new_request()), ' | Processing ', str(
-            self.get_processing_request()), ' | Finalized ',  str(self.get_finalized_request()))
+        print(
+            "New ",
+            str(self.get_new_request()),
+            " | Processing ",
+            str(self.get_processing_request()),
+            " | Finalized ",
+            str(self.get_finalized_request()),
+        )
 
     def run_server(self):
         i = 0
@@ -67,37 +94,47 @@ class Server:
             self.resolve_request()
             self.addNewRequest()
             self.number_of_request_each_time.append(
-                str(len(self.get_new_request()) + len(self.get_processing_request())))
+                str(len(self.get_new_request()) + len(self.get_processing_request()))
+            )
             i += 1
 
     def plot_number_request(self):
         plt.close()
         x_axis = list(range(0, self.iterations))
-        plt.plot(x_axis, self.number_of_request_each_time,
-                 color='blue', label='Cantidad de solicitudes')
+        plt.plot(
+            x_axis,
+            self.number_of_request_each_time,
+            color="blue",
+            label="Cantidad de solicitudes",
+        )
         plt.legend(fancybox=True, framealpha=1, shadow=True, borderpad=1)
-        plt.xlabel('Instantes')
-        plt.ylabel('Estados')
+        plt.xlabel("Instantes")
+        plt.ylabel("Estados")
         plt.show()
 
     def plot_number_request_grouped(self):
         plt.close()
         plt.hist(self.number_of_request_each_time, bins=10)
-        plt.xlabel('Estados')
-        plt.ylabel('Cantidad de solicitudes')
+        plt.xlabel("Estados")
+        plt.ylabel("Cantidad de solicitudes")
         plt.show()
 
     def print_occurrency_percentage(self):
         grouped = Counter(self.number_of_request_each_time).items()
         for obj in grouped:
-            print('Estado ', obj[0], ' = ',
-                  (obj[1] / len(self.number_of_request_each_time)) * 100, '%')
+            print(
+                "Estado ",
+                obj[0],
+                " = ",
+                (obj[1] / len(self.number_of_request_each_time)) * 100,
+                "%",
+            )
 
 
 def main():
     seconds = 1000
-    p_new_request = 1/40
-    p_resolve_request = 1/30
+    p_new_request = 1 / 40
+    p_resolve_request = 1 / 30
     server = Server(seconds, p_new_request, p_resolve_request)
     server.run_server()
     server.print_occurrency_percentage()
